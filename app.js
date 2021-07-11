b6e03875752f50f746d4f1c1e4a673aa2d01bdf3
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-11 20:55:31 +0800

    feat: add search function

--- app.js
+++ app.js
@@ -93,6 +93,24 @@ app.post('/restaurants/:id/delete', (req, res) => {
     .catch(error => console.log(error))
 })
 
+
+////////搜尋餐廳///////////
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const keywordLowercaseNonSpace = keyword.toLowerCase().split(" ").join("")
+  //一次可以搜尋地點,餐廳名,類別
+  return Restaurant.find({
+    $or: [
+      { name: { "$regex": keywordLowercaseNonSpace, "$options": "ix" } },
+      { category: { "$regex": keywordLowercaseNonSpace, "$options": "ix" } },
+      { location: { "$regex": keywordLowercaseNonSpace, "$options": "ix" } }
+    ]
+  })
+    .lean()
+    .then(restaurant => res.render('index', { restaurant, keyword }))
+    .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
