51d493ea2642430ed2b3ab914fd6e685949edb49
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-11 13:32:05 +0800

    feat: add show, index page and css

--- app.js
+++ app.js
@@ -12,15 +12,25 @@ const db = mongoose.connection
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 db.on('error', () => console.log(error))
 db.once('open', () => console.log('mongodb connected'))
 
-
+/////根目錄//////////
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurant => res.render('index', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+////showpage詳細介面////////////
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
 
