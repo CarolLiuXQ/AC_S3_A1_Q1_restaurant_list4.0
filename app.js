b70c0f267472fd9ef9e172dda26f87c8bbbe0ef4
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-13 22:09:52 +0800

    refactor: arrange routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Restaurant = require('./models/restaurant')
 const methodOverride = require('method-override')
+const routes = require('./routes')
+
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
@@ -15,103 +17,12 @@ app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
+app.use(routes)
 
 db.on('error', () => console.log(error))
 db.once('open', () => console.log('mongodb connected'))
 
-/////根目錄//////////
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurant => res.render('index', { restaurant }))
-    .catch(error => console.log(error))
-})
 
-////showpage詳細介面////////////
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-/////新增餐廳/////////////
-app.post('/restaurants/new', (req, res) => {
-  res.render('new')
-})
-//新增之後導去/restaurants做存進database的動作
-app.post('/restaurants', (req, res) => {
-  const restaurant = new Restaurant({
-    name: req.body.name,
-    name_en: req.body.name_en,
-    category: req.body.category,
-    image: req.body.image,
-    location: req.body.location,
-    google_map: req.body.google_map,
-    rating: req.body.rating,
-    phone: req.body.phone,
-    description: req.body.description
-  })
-  return restaurant.save()
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-/////////編輯 page///////////
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-//把編輯過的內容更新進DB
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = req.body.name
-      restaurant.name_en = req.body.name_en
-      restaurant.category = req.body.category
-      restaurant.image = req.body.image
-      restaurant.location = req.body.location
-      restaurant.google_map = req.body.google_map
-      restaurant.rating = req.body.rating
-      restaurant.phone = req.body.phone
-      restaurant.description = req.body.description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
-
-///////刪除餐廳//////////
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-
-////////搜尋餐廳///////////
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const keywordLowercaseNonSpace = keyword.toLowerCase().split(" ").join("")
-  //一次可以搜尋地點,餐廳名,類別
-  return Restaurant.find({
-    $or: [
-      { name: { "$regex": keywordLowercaseNonSpace, "$options": "ix" } },
-      { category: { "$regex": keywordLowercaseNonSpace, "$options": "ix" } },
-      { location: { "$regex": keywordLowercaseNonSpace, "$options": "ix" } }
-    ]
-  })
-    .lean()
-    .then(restaurant => res.render('index', { restaurant, keyword }))
-    .catch(error => console.log(error))
-})
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
