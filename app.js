de024c1115563968a5e4fdaae88a7bab4e54e9fa
DESKTOP-N8M2BVP\s7910 <s7910565@gmail.com>
2021-07-13 22:19:29 +0800

    refactor: arrange db config

--- app.js
+++ app.js
@@ -3,14 +3,10 @@ const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
+require('./config/mongoose')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -18,10 +14,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(routes)
-
-db.on('error', () => console.log(error))
-db.once('open', () => console.log('mongodb connected'))
-
 
 
 app.listen(port, () => {
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,13 +1,8 @@
-const mongoose = require('mongoose')
 const restaurantList = require('../../restaurant.json').results
 const Restaurant = require('../restaurant.js')
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
+const db = require('../../config/mongoose')
 
-db.on('error', () => console.log(error))
 db.once('open', () => {
-  console.log('mongodb connected')
-
   for (let i = 0; i < restaurantList.length; i++) {
     Restaurant.create({
       name: restaurantList[i].name,
