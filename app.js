6808b61ba9da6f2202ce3d773c12948f43ff99fd
CarolLiuXQ <s7910565@gmail.com>
2021-08-12 22:48:39 +0800

    feat: add facebook login&dotenv

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const port = 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const PORT = process.env.PORT
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -19,7 +23,7 @@ app.set('view engine', 'hbs')
 app.set('view engine', 'handlebars')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -41,6 +45,6 @@ app.use((req, res, next) => {
 app.use(routes)
 
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`)
 })
